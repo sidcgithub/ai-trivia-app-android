badecfb41303fa0fb1e2f1d444fcbffb102fcea9
Kazoroo <91783342+Kazoroo@users.noreply.github.com>
2024-05-06 08:49:05 +0200

    Add interface in constructor instead of implementation.
    Now it is repleceable in tests.

--- app/src/main/java/com/triviagenai/triviagen/feature/trivia/domain/usecase/GetTriviaQuestionsUseCase.kt
+++ app/src/main/java/com/triviagenai/triviagen/feature/trivia/domain/usecase/GetTriviaQuestionsUseCase.kt
@@ -5,7 +5,8 @@ import com.triviagenai.triviagen.feature.trivia.domain.model.TriviaQuestion
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetTriviaQuestionsUseCase @Inject constructor(private val repository: TriviaRepositoryImpl) {
+class GetTriviaQuestionsUseCase(
+private val repository: TriviaRepository) {
     operator fun invoke(topic: String): Flow<List<TriviaQuestion>> {
         return repository.fetchTriviaQuestions(topic)
     }
