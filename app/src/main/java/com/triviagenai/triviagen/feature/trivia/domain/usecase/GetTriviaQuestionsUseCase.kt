aeceec502ca7bbe5a8e75220090e9c61f517787d
schordia <siddharth.chordia@gmail.com>
2024-05-06 04:24:13 -0400

    TRIVIAGEN-6: Added import and injection
    Took 4 minutes

--- app/src/main/java/com/triviagenai/triviagen/feature/trivia/domain/usecase/GetTriviaQuestionsUseCase.kt
+++ app/src/main/java/com/triviagenai/triviagen/feature/trivia/domain/usecase/GetTriviaQuestionsUseCase.kt
@@ -1,12 +1,13 @@
 package com.triviagenai.triviagen.feature.trivia.domain.usecase
 
-import com.triviagenai.triviagen.feature.trivia.data.repository.TriviaRepositoryImpl
 import com.triviagenai.triviagen.feature.trivia.domain.model.TriviaQuestion
+import com.triviagenai.triviagen.feature.trivia.domain.repository.TriviaRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetTriviaQuestionsUseCase(
-private val repository: TriviaRepository) {
+class GetTriviaQuestionsUseCase @Inject constructor(
+private val repository: TriviaRepository
+) {
     operator fun invoke(topic: String): Flow<List<TriviaQuestion>> {
         return repository.fetchTriviaQuestions(topic)
     }
