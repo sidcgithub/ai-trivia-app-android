19660abed446b4b7dc10d1166f2323a870e1fb4e
Kazoroo <bczarnecki@duck.com>
2024-05-11 13:53:02 +0200

    Add app primary color, change typography, set app background color to brand color.
    Took 2 hours 34 minutes

--- app/src/main/java/com/triviagenai/triviagen/ui/theme/Type.kt
+++ app/src/main/java/com/triviagenai/triviagen/ui/theme/Type.kt
@@ -1,34 +1,55 @@
 package com.triviagenai.triviagen.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.triviagenai.triviagen.R
 
-// Set of Material typography styles to start with
+
+val karmaFontFamily = FontFamily(
+    Font(R.font.karma_bold, FontWeight.Bold),
+    Font(R.font.karma_light, FontWeight.Light),
+    Font(R.font.karma_medium, FontWeight.Medium),
+    Font(R.font.karma_regular, FontWeight.Normal),
+    Font(R.font.karma_semibold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = karmaFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        letterSpacing = 0.5.sp,
+        color = Color.White
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontFamily = karmaFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 32.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
+        color = Color.White
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = karmaFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 0.5.sp,
+        color = Color.White
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = karmaFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 26.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        color = Color.White
+    ),
 )
