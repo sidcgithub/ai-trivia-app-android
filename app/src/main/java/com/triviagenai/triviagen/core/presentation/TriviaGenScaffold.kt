7ffcfba609fb67e760e83086837d5c984aeb0b13
Kazoroo <bczarnecki@duck.com>
2024-05-11 14:04:35 +0200

    Add main menu screen. Create reusable Scaffold function.
    Took 10 minutes

---
+++ app/src/main/java/com/triviagenai/triviagen/core/presentation/TriviaGenScaffold.kt
@@ -0,0 +1,38 @@
+package com.triviagenai.triviagen.core.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.triviagenai.triviagen.R
+import com.triviagenai.triviagen.ui.theme.RoyalPurple
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TriviaGenScaffold(
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name)
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = RoyalPurple)
+            )
+        }
+    ) {
+        Box(Modifier.fillMaxSize().padding(it)) {
+            content()
+        }
+    }
+}
---
+++ app/src/main/java/com/triviagenai/triviagen/trivia/presentation/mainmenu/MainMenuScreen.kt
@@ -0,0 +1,60 @@
+package com.triviagenai.triviagen.trivia.presentation.mainmenu
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.triviagenai.triviagen.MainActivity
+import com.triviagenai.triviagen.R
+import com.triviagenai.triviagen.core.presentation.TriviaGenScaffold
+
+@Composable
+fun MainMenuScreen() {
+    TriviaGenScaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.yellow_app_logo),
+                contentDescription = "App logo",
+                modifier = Modifier
+                    .size(200.dp)
+            )
+            
+            Spacer(modifier = Modifier.height(50.dp))
+            
+            TextButton(
+                onClick = { /*navigates to the trivia screen*/ }
+            ) {
+                Text(stringResource(R.string.quick_game))
+            }
+
+            TextButton(
+                onClick = { /*navigates to the options screen*/ }
+            ) {
+                Text(stringResource(R.string.options))
+            }
+
+            val activity = LocalContext.current as MainActivity
+
+            TextButton(
+                onClick = { activity.finish() }
+            ) {
+                Text(stringResource(R.string.exit))
+            }
+        }
+     }
+}
