77db40131fdd6f83650f7bcec54b361c1c72bcdf
Kazoroo <bczarnecki@duck.com>
2024-05-12 19:16:06 +0200

    Optimize imports
    Took 41 seconds

--- app/src/main/java/com/triviagenai/triviagen/MainActivity.kt
+++ app/src/main/java/com/triviagenai/triviagen/MainActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.triviagenai.triviagen.trivia.presentation.mainmenu.MainMenuScreen
 import com.triviagenai.triviagen.ui.theme.TriviaGenTheme
 import dagger.hilt.android.AndroidEntryPoint
