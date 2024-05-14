3391dba0376c78db3a698b69340de21f0936f020
Kazoroo <bczarnecki@duck.com>
2024-05-14 08:51:27 +0200

    Create TriviaGameScreen, optimize imports for RoundSetupScreen, change theme primary color, change parameter answer to integer in TriviaQuestion data class.
    Took 59 minutes

--- app/src/main/java/com/triviagenai/triviagen/trivia/domain/model/TriviaQuestion.kt
+++ app/src/main/java/com/triviagenai/triviagen/trivia/domain/model/TriviaQuestion.kt
@@ -5,7 +5,7 @@ import com.triviagenai.triviagen.trivia.data.model.Round
 data class TriviaQuestion(
     val question: String,
     val options: List<String>,
-    val answer: String
+    val answer: Int
 )
 
 fun Round.TriviaRound.mapToTriviaQuestions() = questions.map { question ->
