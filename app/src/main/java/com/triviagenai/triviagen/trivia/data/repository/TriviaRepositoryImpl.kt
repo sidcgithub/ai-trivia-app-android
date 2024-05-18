5db2437a1247e95ce750d546c9e673317dcbf40f
sidcgithub <siddharth.chordia@gmail.com>
2024-05-18 14:45:29 -0400

    TRIVIAGEN-14: Network wiring and ViewModel logic (#19)
    * TRIVIAGEN-14: Network wiring and ViewModel logic
    
    * TRIVIAGEN-14: Refactored UI state
    
    * TRIVIAGEN-14: Lint
    
    * TRIVIAGEN-14: Moved points to viewmodel

--- app/src/main/java/com/triviagenai/triviagen/trivia/data/repository/TriviaRepositoryImpl.kt
+++ app/src/main/java/com/triviagenai/triviagen/trivia/data/repository/TriviaRepositoryImpl.kt
@@ -1,16 +1,27 @@
 package com.triviagenai.triviagen.trivia.data.repository
 
 import com.triviagenai.triviagen.core.data.api.TriviaGenApiService
-import com.triviagenai.triviagen.trivia.domain.model.TriviaQuestion
+import com.triviagenai.triviagen.core.data.model.Resource
+import com.triviagenai.triviagen.trivia.data.model.Round
 import com.triviagenai.triviagen.trivia.domain.repository.TriviaRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class TriviaRepository @Inject constructor(private val apiService: TriviaGenApiService) :
+class TriviaRepositoryImpl @Inject constructor(private val apiService: TriviaGenApiService) :
     TriviaRepository {
 
-    override fun fetchTriviaQuestions(category: String): Flow<List<TriviaQuestion>> = flow {
-        apiService.getTriviaQuestions(category)
+    override fun fetchTriviaQuestions(topic: String): Flow<Resource<out Round>> = flow {
+        try {
+            emit(Resource.Loading())
+            val result = when (val response = apiService.getTriviaQuestions(topic)) {
+                is Round.TriviaRound -> Resource.Success(data = response)
+                is Round.Error -> Resource.Error(errorMessage = response.message)
+                else -> Resource.Error("Something went wrong...")
+            }
+            emit(result)
+        } catch (e: Exception) {
+            emit(Resource.Error("Exception"))
+        }
     }
 }
