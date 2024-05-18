822fe564bb74702f2be8ccdf57bbe5a16965e291
schordia <siddharth.chordia@gmail.com>
2024-05-18 06:18:06 -0400

    TRIVIAGEN-14: Lint

--- app/src/main/java/com/triviagenai/triviagen/trivia/presentation/TriviaUIState.kt
+++ app/src/main/java/com/triviagenai/triviagen/trivia/presentation/TriviaUIState.kt
@@ -4,6 +4,11 @@ import com.triviagenai.triviagen.trivia.domain.model.TriviaQuestion
 
 sealed class TriviaUIState {
     object Loading : TriviaUIState()
-    data class Success(val questions: List<TriviaQuestion>, val currentQuestionIndex: Int, val score: Int) : TriviaUIState()
+    data class Success(
+        val questions: List<TriviaQuestion>,
+        val currentQuestionIndex: Int,
+        val score: Int
+    ) : TriviaUIState()
+
     data class Error(val message: String) : TriviaUIState()
 }
