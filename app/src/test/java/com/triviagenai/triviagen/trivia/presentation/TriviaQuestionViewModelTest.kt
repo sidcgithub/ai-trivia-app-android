7c3ee01b32c1e171c3fba62fd493efc1c9adc9c4
schordia <siddharth.chordia@gmail.com>
2024-06-14 15:10:56 -0400

    TRIVIAGEN-24: Viewmodel tests

--- app/src/test/java/com/triviagenai/triviagen/trivia/presentation/TriviaQuestionViewModelTest.kt
+++ app/src/test/java/com/triviagenai/triviagen/trivia/presentation/TriviaQuestionViewModelTest.kt
@@ -2,10 +2,14 @@ package com.triviagenai.triviagen.trivia.presentation
 
 import com.triviagenai.triviagen.core.testing.MainDispatcherRule
 import com.triviagenai.triviagen.core.testing.repository.TestTriviaRepositoryImpl
+import com.triviagenai.triviagen.trivia.domain.model.SelectedAnswerState
 import com.triviagenai.triviagen.trivia.domain.usecase.GetTriviaQuestionsUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.*
-
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -25,24 +29,45 @@ class TriviaQuestionViewModelTest {
         viewModel = TriviaQuestionViewModel(triviaQuestionsUseCase)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun testFetchTriviaQuestions() = runTest {
-        // Test with valid topic
+    fun `test fetch valid scenario`() = runTest {
+        val collectJob = launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
         viewModel.fetchTriviaQuestions("History")
         assert(viewModel.uiState.value is TriviaUIState.Success)
-
-        // Test with invalid topic
-        viewModel.fetchTriviaQuestions("")
-        assert(viewModel.uiState.value is TriviaUIState.Error)
+        collectJob.cancel()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun testProcessIntent() = runTest {
-        viewModel.processIntent(
-            TriviaIntent.SubmitAnswer(0)
-                    assert (viewModel.uiState.value is TriviaUIState.Success)
+    fun `test fetch invalid scenario`() = runTest {
+        val collectJob = launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+        viewModel.fetchTriviaQuestions("")
+        assert(viewModel.uiState.value is TriviaUIState.Error)
+        collectJob.cancel()
+    }
 
-                    viewModel . processIntent (TriviaIntent.RandomTriviaRound)
-                    assert (viewModel.uiState.value is TriviaUIState.Success)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test process intent scenarios`() = runTest {
+        val collectJob = launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+        viewModel.fetchTriviaQuestions("History")
+        viewModel.processIntent(TriviaIntent.SubmitAnswer(0))
+        assert(
+            viewModel.uiState.value is TriviaUIState.Success
+                    && (viewModel.uiState.value as
+                    TriviaUIState.Success)
+                .questions[(viewModel.uiState.value as TriviaUIState.Success)
+                .currentQuestionIndex].selectedAnswer == SelectedAnswerState.Answered(0)
+        )
+
+        viewModel.processIntent(TriviaIntent.RandomTriviaRound)
+        assert(viewModel.uiState.value is TriviaUIState.Success)
+        collectJob.cancel()
+    }
+
+    @After
+    fun tearDown() {
+        viewModel.reset()
     }
 }
