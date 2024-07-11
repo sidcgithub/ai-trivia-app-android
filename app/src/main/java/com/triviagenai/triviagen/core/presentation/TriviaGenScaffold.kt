c36c7ba647557d455016247c40d1f44f9717f270
Kazoroo <kazoroo1903@gmail.com>
2024-07-11 13:33:48 +0200

    Add test for top app bar back button in TriviaGameScreen

--- app/src/main/java/com/triviagenai/triviagen/core/presentation/TriviaGenScaffold.kt
+++ app/src/main/java/com/triviagenai/triviagen/core/presentation/TriviaGenScaffold.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -40,6 +41,7 @@ fun TriviaGenScaffold(
                             onClick = {
                                 navigationStatus.backNav()
                             },
+                            modifier = Modifier.testTag("TopAppBarBackNavigationButton")
                         ) {
                             Icon(
                                 imageVector = Icons.Default.ArrowBack,
--- app/src/main/java/com/triviagenai/triviagen/trivia/presentation/triviagame/components/QuitTriviaAlertDialog.kt
+++ app/src/main/java/com/triviagenai/triviagen/trivia/presentation/triviagame/components/QuitTriviaAlertDialog.kt
@@ -2,19 +2,17 @@ package com.triviagenai.triviagen.trivia.presentation.triviagame.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.triviagenai.triviagen.R
-import com.triviagenai.triviagen.core.presentation.navigation.Route
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun QuitTriviaAlertDialog(
     isShowingExitDialog: MutableState<Boolean>,
@@ -29,6 +27,7 @@ fun QuitTriviaAlertDialog(
                         isShowingExitDialog.value = false
                         navController.navigateUp()
                     },
+                    modifier = Modifier.testTag("QuitTriviaAlertDialogConfirmButton")
                 ) {
                     Text(text = stringResource(R.string.yes))
                 }
